21ba01b6394279c5839d85b5abf42f939c3f4de0
Katerina-il <kkaterina.iln@gmail.com>
2025-01-03 18:49:34 +0300

    Add files via upload

---
+++ module_8_1_variant2.py
@@ -0,0 +1,23 @@
+"""
+Домашнее задание по теме "Try и Except"
+Задание "Программистам всё можно":
+
+Пора разрушать шаблоны привычного нам Python! Вот вас раздражает,
+что мы не можем сложить число(int) и строку(str)? Давайте исправим это недоразумение!
+
+"""
+
+
+
+def add_everything_up(number1, number2):
+    """будет складывать числа(int, float) и строки(str)"""
+    # result = number1 + number2
+    try:
+        return number1 + number2
+    except TypeError as exc:
+        return f"{number1}{number2}"
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
